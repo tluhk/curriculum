e3d8d618d64a4acd9b81d4e2b8331dd22f991200
Martti Raavel <mrt@mrt.ee>
2025-02-28 12:04:51 +0200

    Fix base path in Vite configuration for consistency

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Curriculum/', // Set the base to match your repository name
+  base: '/curriculum/', // Set the base to match your repository name
   build: {
     assetsDir: 'assets', // Ensure assets are placed in the assets directory
     outDir: 'dist', // Output directory
